28a49ef5d90b5fcc9bc0ecde78b084763690358b
dziugas.garla <dziugas.garla@stud.vilniustech.lt>
2024-03-07 17:33:32 +0200

    Add unit tests to NumberService

---
+++ UnitTests/BusinessLayerTests/MoqSetups/FileIOManagerMoqSetups.cs
@@ -0,0 +1,46 @@
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using Core;
+using Core.Exceptions;
+using Moq;
+
+namespace UnitTests.BusinessLayerTests.MoqSetups;
+
+public static class FileIOManagerMoqSetups
+{
+    public static Mock<IFileIOManager> Setup_WriteString_DoesntThrow(this Mock<IFileIOManager> mockRepository)
+    {
+        mockRepository
+           .Setup(m => m.WriteStringAsync(It.IsAny<string>()))
+           .Returns(Task.CompletedTask);
+
+        return mockRepository;
+    }
+
+    public static Mock<IFileIOManager> Setup_WriteString_ThrowsFileCouldNotBeWritten(this Mock<IFileIOManager> mockRepository)
+    {
+        mockRepository
+           .Setup(m => m.WriteStringAsync(It.IsAny<string>()))
+           .ThrowsAsync(new HttpResponseException(HttpStatusCode.InternalServerError, "The file could not be written. Try to check if path exist: ____"));
+
+        return mockRepository;
+    }
+
+    public static Mock<IFileIOManager> Setup_ReadString_ReturnString(this Mock<IFileIOManager> mockRepository, string str)
+    {
+        mockRepository
+           .Setup(m => m.ReadStringAsync())
+           .ReturnsAsync(str);
+
+        return mockRepository;
+    }
+
+    public static Mock<IFileIOManager> Setup_ReadString_ThrowsFileCouldNotBeRead(this Mock<IFileIOManager> mockRepository)
+    {
+        mockRepository
+           .Setup(m => m.ReadStringAsync())
+           .ThrowsAsync(new HttpResponseException(HttpStatusCode.InternalServerError, "The file could not be read. Try to check if file or path exist: _____"));
+
+        return mockRepository;
+    }
+}
--- UnitTests/BusinessLayerTests/NumberServiceTests.cs
+++ UnitTests/BusinessLayerTests/NumberServiceTests.cs
@@ -1,5 +1,141 @@
+using BusinessLayer.BusinessServices;
+using Core;
+using Core.Exceptions;
+using FluentAssertions;
+using Moq;
+using UnitTests.BusinessLayerTests.MoqSetups;
+
+namespace UnitTests.BusinessLayerTests;
+
 [TestFixture]
 public class RoomServicesTests
 {
+    private MockRepository mockRepository;
 
+    private Mock<IFileIOManager> _mockFileIOManager;
+
+    [SetUp]
+    public void SetUp()
+    {
+        mockRepository = new MockRepository(MockBehavior.Strict);
+
+        _mockFileIOManager = mockRepository.Create<IFileIOManager>();
+    }
+
+    private NumberService InitializeNumberServices()
+    {
+        return new NumberService(
+            _mockFileIOManager.Object
+        );
+    }
+
+    [Test]
+    [TestCase("1")]
+    [TestCase("1 2 3")]
+    [TestCase("1 3 2")]
+    [TestCase("1 3 3")]
+    [TestCase("1 30000 3")]
+    public async Task CreateNumbersAsync_GiveValidNumbers_DoesntThrow(string numbers)
+    {
+        // Arrange
+        _mockFileIOManager
+           .Setup_WriteString_DoesntThrow();
+
+        var numberService = InitializeNumberServices();
+
+        // Act
+        var action = () => numberService.CreateNumbersAsync(numbers);
+
+        // Assert
+        await action.Should().NotThrowAsync();
+    }
+
+    [Test]
+    [TestCase("1", "1")]
+    [TestCase("1 2 3", "1 2 3")]
+    [TestCase("1 3 2", "1 2 3")]
+    [TestCase("2 3 2", "2 2 3")]
+    [TestCase("-1 1 0", "-1 0 1")]
+    public async Task CreateNumbersAsync_GiveValidNumbers_ReturnsSortedNumbers(string givenNumbers, string expectedResult)
+    {
+        // Arrange
+        _mockFileIOManager
+           .Setup_WriteString_DoesntThrow();
+
+        var numberService = InitializeNumberServices();
+
+        // Act
+        var result = await numberService.CreateNumbersAsync(givenNumbers);
+
+        // Assert
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+    [Test]
+    [TestCase(" 1")]
+    [TestCase("")]
+    [TestCase("01")]
+    [TestCase("-01 1 0")]
+    public void CreateNumbersAsync_GiveInvalidNumbers_ThrowsValidationException(string givenNumbers)
+    {
+        // Arrange
+        _mockFileIOManager
+           .Setup_WriteString_DoesntThrow();
+
+        var numberService = InitializeNumberServices();
+
+        // Act
+        var action = () => numberService.CreateNumbersAsync(givenNumbers);
+
+        // Assert
+        var ex = action.Should().ThrowAsync<ValidationException>();
+    }
+
+    [Test]
+    public async Task CreateNumbersAsync_GiveValidNumbers_ThrowsFileCouldNotBeWritten()
+    {
+        // Arrange
+        _mockFileIOManager
+           .Setup_WriteString_ThrowsFileCouldNotBeWritten();
+
+        var numberService = InitializeNumberServices();
+
+        // Act
+        var action = () => numberService.CreateNumbersAsync("1 2 3");
+
+        // Assert
+        await action.Should().ThrowAsync<HttpResponseException>().WithMessage("The file could not be written. Try to check if path exist: ____");
+    }
+
+    [Test]
+    public async Task GetNumbersAsync_DoesntThrow()
+    {
+        // Arrange
+        _mockFileIOManager
+           .Setup_ReadString_ReturnString("1 2 3");
+
+        var numberService = InitializeNumberServices();
+
+        // Act
+        var action = () => numberService.GetNumbersAsync();
+
+        // Assert
+        await action.Should().NotThrowAsync();
+    }
+
+    [Test]
+    public async Task GetNumbersAsync_ThrowsFileCouldNotBeRead()
+    {
+        // Arrange
+        _mockFileIOManager
+           .Setup_ReadString_ThrowsFileCouldNotBeRead();
+
+        var numberService = InitializeNumberServices();
+
+        // Act
+        var action = () => numberService.GetNumbersAsync();
+
+        // Assert
+        await action.Should().ThrowAsync<HttpResponseException>().WithMessage("The file could not be read. Try to check if file or path exist: _____");
+    }
 }
